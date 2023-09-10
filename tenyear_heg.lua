f8f1223f3f8cb9140afaf8cab24735a8eb28b936
s1134s <9902511+s1134s@user.noreply.gitee.com>
2023-09-10 14:04:07 +0000

    update tenyear_heg.lua. fix xunchen 6.
    Signed-off-by: s1134s <9902511+s1134s@user.noreply.gitee.com>

--- tenyear_heg.lua
+++ tenyear_heg.lua
@@ -721,12 +721,12 @@ local fenglve = fk.CreateActiveSkill{
       local dummy2 = Fk:cloneCard("dilu")
       if #player:getCardIds{Player.Hand, Player.Equip} < 2 then
         dummy2:addSubcards(target:getCardIds{Player.Hand, Player.Equip})
+        if #dummy2.subcards > 0 then
+            room:obtainCard(target, dummy2, false, fk.ReasonGive)
+        end
       else
-        local cards2 = room:askForCardsChosen(player, player, 1, 1, "he", self.name)
-        dummy2:addSubcards(cards2)
-      end
-      if #dummy2.subcards > 0 then
-        room:obtainCard(target, dummy2, false, fk.ReasonGive)
+        local cards2 = room:askForCardChosen(player, player, "he", self.name)
+        room:obtainCard(target, cards2, false, fk.ReasonGive)
       end
     end
     local choices = {"ty_heg__fenglve_mn_ask::" .. target.id, "Cancel"}
@@ -759,12 +759,12 @@ local fenglve_mn = fk.CreateActiveSkill{
       local dummy1 = Fk:cloneCard("dilu")
       if #target:getCardIds{Player.Hand, Player.Equip, Player.Judge} < 2 then
         dummy1:addSubcards(target:getCardIds{Player.Hand, Player.Equip, Player.Judge})
+        if #dummy1.subcards > 0 then
+            room:obtainCard(player, dummy1, false, fk.ReasonGive)
+        end
       else
         local cards1 = room:askForCardsChosen(target, target, 1, 1, "hej", self.name)
-        dummy1:addSubcards(cards1)
-      end
-      if #dummy1.subcards > 0 then
-        room:obtainCard(player, dummy1, false, fk.ReasonGive)
+        room:obtainCard(player, cards1, false, fk.ReasonGive)
       end
     elseif pindian.results[target.id].winner == target then
       local dummy2 = Fk:cloneCard("dilu")
@@ -802,7 +802,7 @@ xunchen:addSkill(fenglve)
 Fk:loadTranslationTable{
   ["ty_heg__xunchen"] = "荀谌",
   ["ty_heg__fenglve"] = "锋略",
-  ["ty_heg__fenglve__mn"] = "锋略：选择一名其他角色与其拼点，若你赢，该角色交给你其区域内的一张牌；若其赢，你交给其两张牌。，",
+  ["ty_heg__fenglve__mn"] = "锋略：选择一名其他角色与其拼点，若你赢，该角色交给你其区域内的一张牌；若其赢，你交给其两张牌。",
   [":ty_heg__fenglve"] = "出牌阶段限一次，你可以和一名其他角色拼点，若你赢，该角色交给你其区域内的两张牌；若其赢，你交给其一张牌。"..
   "<br><font color=\"blue\">◆纵横：交换〖锋略〗描述中的“一张牌”和“两张牌”。<font><br><font color=\"grey\"><b>纵横</b>："..
   "当拥有“纵横”效果技能发动结算完成后，可以令技能目标角色获得对应修订描述后的技能，直到其下回合结束。",
