4db1d2d383e913f4966b8ebfd5822a0517bdfc0e
seven <786852516@qq.com>
2025-06-07 08:56:07 +0800

    除疠

--- pkg/standard/skills/chuli.lua
+++ pkg/standard/skills/chuli.lua
@@ -4,6 +4,7 @@ local chuli = fk.CreateSkill{
 }
 local H = require "packages/hegemony/util"
 chuli:addEffect("active", {
+  prompt = "#hs__chuli",
   anim_type = "control",
   card_num = 0,
   min_target_num = 1,
@@ -17,25 +18,25 @@ chuli:addEffect("active", {
   end,
   on_use = function(self, room, effect)
     local player = effect.from
-    local targets = table.clone(effect.tos)
+    local targets = table.simpleClone(effect.tos)
     table.insert(targets, 1, player)
     room:sortByAction(targets)
+    local draw = {}
     for _, tar in ipairs(targets) do
       if not tar:isNude() then
         local c = room:askToChooseCard(player, {
-    target = tar,
-    flag = "he",
-    skill_name = chuli.name,
-  })
+          target = tar,
+          flag = "he",
+          skill_name = chuli.name,
+        })
         room:throwCard({c}, chuli.name, tar, player)
         if Fk:getCardById(c).suit == Card.Spade then
-          room:addPlayerMark(tar, "_hs__chuli-phase", 1)
+          table.insert(draw, tar)
         end
       end
     end
     for _, tar in ipairs(targets) do
-      if tar:getMark("_hs__chuli-phase") > 0 and not tar.dead then
-        room:setPlayerMark(tar, "_hs__chuli-phase", 0)
+      if table.contains(draw, tar) and not tar.dead then
         tar:drawCards(1, chuli.name)
       end
     end
@@ -45,6 +46,7 @@ chuli:addEffect("active", {
 Fk:loadTranslationTable{
   ["hs__chuli"] = "除疠",
   [":hs__chuli"] = "出牌阶段限一次，你可选择至多三名势力各不相同或未确定势力的其他角色，然后你弃置你和这些角色的各一张牌。被弃置♠牌的角色各摸一张牌。",
+  ["#hs__chuli"] = "除疠:择至多三名势力各不相同或未确定势力的其他角色，弃置你和这些角色的各一张牌",
   ["$hs__chuli1"] = "病去，如抽丝。",
   ["$hs__chuli2"] = "病入膏肓，需下猛药。",
 }
